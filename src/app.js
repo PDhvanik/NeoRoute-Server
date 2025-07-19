de4361a4e1b900082fc4877228449fb9d36e9c31
Dhvanik Patel <pateldhvanik789@gmail.com>
2025-07-19 19:45:31 +0530

    Bug fix

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import pathRoutes from "./routes/pathRoutes.js";
 const app = express();
 
 app.use(cors({
-   origin:"https://neo-route-client.vercel.app/"
+   origin:"https://neo-route-client.vercel.app"
 }
 ));
 app.use(express.json());
