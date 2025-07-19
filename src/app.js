2756a3c348f143a1960de9136dea07b8c3f84a8b
Dhvanik Patel <pateldhvanik789@gmail.com>
2025-07-19 19:45:06 +0530

    Update CORS origin for security

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ import pathRoutes from "./routes/pathRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+   origin:"https://neo-route-client.vercel.app/"
+}
+));
 app.use(express.json());
 app.use("/api", pathRoutes);
 
